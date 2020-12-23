eb9948a54252e54b153205660733743996e64f79
Steven Webster <steven@is-just-a.dev>
2020-12-23 10:37:56 -0500

    Small fixes

--- Subclass.cs
+++ Subclass.cs
@@ -29,7 +29,7 @@ namespace Subclass
         public override PluginPriority Priority { get; } = PluginPriority.Last;
         public override string Name { get; } = "Subclass";
         public override string Author { get; } = "Steven4547466";
-        public override Version Version { get; } = new Version(1, 2, 7);
+        public override Version Version { get; } = new Version(1, 2, 8);
         public override Version RequiredExiledVersion { get; } = new Version(2, 1, 19);
         public override string Prefix { get; } = "Subclass";
 
