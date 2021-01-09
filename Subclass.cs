f1f0bfc93ff6d31ca8da6f8129e715184ac77f7b
Steven Webster <steven@is-just-a.dev>
2021-01-09 09:48:18 -0500

    Fix prygates

--- Subclass.cs
+++ Subclass.cs
@@ -29,8 +29,8 @@ namespace Subclass
         public override PluginPriority Priority { get; } = PluginPriority.Last;
         public override string Name { get; } = "Subclass";
         public override string Author { get; } = "Steven4547466";
-        public override Version Version { get; } = new Version(1, 2, 13);
-        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 25);
+        public override Version Version { get; } = new Version(1, 2, 14);
+        public override Version RequiredExiledVersion { get; } = new Version(2, 1, 28);
         public override string Prefix { get; } = "Subclass";
 
         public Handlers.Player player { get; set; }
