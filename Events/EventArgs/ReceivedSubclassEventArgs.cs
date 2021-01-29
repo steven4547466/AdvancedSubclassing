b7b06ad4823d99b5a7f00120de3acb7f05613f82
Steven Webster <steven@is-just-a.dev>
2021-01-28 20:53:53 -0500

    Docs, I guess

--- Events/EventArgs/ReceivedSubclassEventArgs.cs
+++ Events/EventArgs/ReceivedSubclassEventArgs.cs
@@ -7,8 +7,16 @@ using Exiled.API.Features;
 
 namespace Subclass.Events.EventArgs
 {
+	/// <summary>
+	/// Contains the information after a player receives a subclass.
+	/// </summary>
 	public class ReceivedSubclassEventArgs : System.EventArgs
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReceivedSubclassEventArgs"/> class.
+		/// </summary>
+		/// <param name="player">The <see cref="Exiled.API.Features.Player"/> that received the subclass</param>
+		/// <param name="subClass">The <see cref="SubClass"/> the player got.</param>
 		public ReceivedSubclassEventArgs(Player player, SubClass subClass)
 		{
 			Player = player;
--- Events/EventArgs/ReceivingSubclassEventArgs.cs
+++ Events/EventArgs/ReceivingSubclassEventArgs.cs
@@ -7,8 +7,17 @@ using Exiled.API.Features;
 
 namespace Subclass.Events.EventArgs
 {
+	/// <summary>
+	/// Contains the information before a player receives a subclass.
+	/// </summary>
 	public class ReceivingSubclassEventArgs : System.EventArgs
 	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ReceivingSubclassEventArgs"/> class.
+		/// </summary>
+		/// <param name="player">The <see cref="Exiled.API.Features.Player"/> that received the subclass</param>
+		/// <param name="subClass">The <see cref="SubClass"/> the player got.</param>
+		/// <param name="isAllowed">Whether or not this player should receive the subclass.</param>
 		public ReceivingSubclassEventArgs(Player player, SubClass subClass, bool isAllowed = true)
 		{
 			Player = player;
